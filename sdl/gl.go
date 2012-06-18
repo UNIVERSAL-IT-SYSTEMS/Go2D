9458c26a7be760b8d82d53a6f970a3d96395a3f6
Nick Sarten <gen.battle@gmail.com>
2012-06-19 00:08:12 +1200

    Added GL_SetSwapInterval function

--- sdl/gl.go
+++ sdl/gl.go
@@ -4,6 +4,7 @@ package sdl
 // #cgo CFLAGS: -D_REENTRANT
 // #include <SDL2/SDL.h>
 import "C"
+import "errors"
 
 // This file contains OpenGL-specific functions from SDL.
 
@@ -12,4 +13,11 @@ type GLContext C.SDL_GLContext
 func GL_CreateContext(_win *Window) *GLContext {
 	context := GLContext(C.SDL_GL_CreateContext(_win.window))
 	return &context
+}
+
+func GL_SetSwapInterval(_interval int) error {
+	if C.SDL_GL_SetSwapInterval(C.int(_interval)) != 0 {
+		return errors.New(GetError())
+	}
+	return nil
 }
